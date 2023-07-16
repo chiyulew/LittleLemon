024ba09bd8fa36a12cd846d98f3203749c399f0e
chiyulew <zl93@cornell.edu>
2023-07-16 09:41:56 -0400

    setting up models

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    bookingDate = models.DateTimeField()
+    
+    
 class Menu(models.Model):
-    title = models.CharField(max_length=100)
+    id = models.SmallIntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    inventory = models.SmallIntegerField(default=0)
